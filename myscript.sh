1f616e7d52e5f71aa9412b634eadd4f8bef19e48
Zorba11 <zorbathegreek111@gmail.com>
2022-03-29 10:13:51 -0600

    echo, variables, conditionals

---
+++ myscript.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+# ECHO COMMAND
+# echo Hello World!
+
+#VARIABLES
+#Uppercase by convention
+# Letters, numbers, underscores
+NAME="Bob"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
+
+#USER INPUT
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you"
+
+# SIMPLE IF STATEMENT
+# if [ "$NAME" == "Alen" ]
+# then
+#   echo "Your name is alen"
+# fi
+
+# IF-ELSE
+# if [ "$NAME" == "Alen" ]
+# then
+#   echo "Your name is alen"
+# else
+#   echo "Your name is not alen"
+# fi
+
+# ELSE-IF (elif)
+# if [ "$NAME" == "Alen" ]
+# then
+#   echo "Your name is alen"
+# elif [ "$NAME" == "Jack" ]
+# then
+#   echo "Your name is Jack"
+# else
+#   echo "Your name is not jack or alen"
+# fi
+
+# COMPARISON
